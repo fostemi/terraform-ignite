f325c4ee265ce650d6048467007cd068aaca68cc
fosterm-mw <michael.foster@mavenwave.com>
2022-06-16 17:31:50 -0500

    creating a vpc

--- network.tf
+++ network.tf
@@ -1,7 +1,7 @@
 module "vpc" {
   source                                 = "terraform-google-modules/network/google//modules/vpc"
   project_id                             = "terraform-ignite"
-  network_name                           = "terraform-ignite"
+  network_name                           = "terraform-test"
   routing_mode                           = "REGIONAL"
   shared_vpc_host                        = false
   delete_default_internet_gateway_routes = false

06d1f36ba44d4429a4ee4318f37fdb000daa11a9
fosterm-mw <michael.foster@mavenwave.com>
2022-06-16 17:22:16 -0500

    changed settings

--- network.tf
+++ network.tf
@@ -2,7 +2,7 @@ module "vpc" {
   source                                 = "terraform-google-modules/network/google//modules/vpc"
   project_id                             = "terraform-ignite"
   network_name                           = "terraform-ignite"
-  routing_mode                           = "GLOBAL"
+  routing_mode                           = "REGIONAL"
   shared_vpc_host                        = false
   delete_default_internet_gateway_routes = false
 }

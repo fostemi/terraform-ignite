0b7f926ab65fb0cb93151a5ddd05b7a528f4341e
fosterm-mw <michael.foster@mavenwave.com>
2022-06-17 23:51:05 -0500

    different file blueprint

--- gke.tf
+++ gke.tf
@@ -1,87 +1,38 @@
 module "gke" {
-  source                     = "terraform-google-modules/kubernetes-engine/google//modules/beta-private-cluster-update-variant"
-  project_id                 = "terraform-ignite"
-  name                       = "terraform-gke"
-  region                     = "us-central1"
-  zones                      = ["us-central1-a", "us-central1-b", "us-central1-f"]
-  network                    = "vpc-01"
-  subnetwork                 = "us-central1-01"
-  ip_range_pods              = "us-central1-01-gke-01-pods"
-  ip_range_services          = "us-central1-01-gke-01-services"
-  http_load_balancing        = false
-  network_policy             = false
-  horizontal_pod_autoscaling = true
-  filestore_csi_driver       = false
-  enable_private_endpoint    = true
-  enable_private_nodes       = true
-  master_ipv4_cidr_block     = "10.0.0.0/28"
-  istio                      = true
-  cloudrun                   = true
-  dns_cache                  = false
 
-  node_pools = [
-    {
-      name                      = "default-node-pool"
-      machine_type              = "e2-medium"
-      node_locations            = "us-central1-b,us-central1-c"
-      min_count                 = 1
-      max_count                 = 100
-      local_ssd_count           = 0
-      spot                      = false
-      local_ssd_ephemeral_count = 0
-      disk_size_gb              = 100
-      disk_type                 = "pd-standard"
-      image_type                = "COS_CONTAINERD"
-      enable_gcfs               = false
-      auto_repair               = true
-      auto_upgrade              = true
-      service_account           = "terraform-expert@terraform-ignite.iam.gserviceaccount.com"
-      preemptible               = false
-      initial_node_count        = 80
-    },
+  source = "./modules/terraform-google-gke"
+
+  use_gke    = var.use_gke
+  use_anthos = var.use_anthos
+
+  regional           = false
+  network_project_id = "terraform-ignite"
+  gke_project_id     = "terraform-ignite"
+  gke_node_sa        = "terraform-expert@terraform-ignite.iam.gserviceaccount.com"
+
+  cluster_name        = "cluster1"
+  network_name        = "terraform-test"
+  subnet_name         = "subnet1"
+  region              = "us-central1"
+  zones               = "us-central1-c"
+  pods_range_name     = local.pods_range_name
+  services_range_name = local.services_range_name
+  master_range        = local.master_range
+  machine_type        = var.machine_type
+
+  # failover_cluster_name        = local.failover_cluster_name
+  # failover_subnet_name         = local.failover_subnet_name
+  failover_region              = local.failover_region
+  failover_zones               = local.failover_zones
+  failover_pods_range_name     = local.failover_pods_range_name
+  failover_services_range_name = local.failover_services_range_name
+  failover_master_range        = local.failover_master_range
+
+  config_connector = true
+
+  depends_on = [
+    module.vpc,
+    module.subnets,
   ]
 
-  node_pools_oauth_scopes = {
-    all = []
-
-    default-node-pool = [
-      "https://www.googleapis.com/auth/cloud-platform",
-    ]
-  }
-
-  node_pools_labels = {
-    all = {}
-
-    default-node-pool = {
-      default-node-pool = true
-    }
-  }
-
-  node_pools_metadata = {
-    all = {}
-
-    default-node-pool = {
-      node-pool-metadata-custom-value = "my-node-pool"
-    }
-  }
-
-  node_pools_taints = {
-    all = []
-
-    default-node-pool = [
-      {
-        key    = "default-node-pool"
-        value  = true
-        effect = "PREFER_NO_SCHEDULE"
-      },
-    ]
-  }
-
-  node_pools_tags = {
-    all = []
-
-    default-node-pool = [
-      "default-node-pool",
-    ]
-  }
 }

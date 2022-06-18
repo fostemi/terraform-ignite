b3f600398d278b9ae43782dfecb3bdb270c94ece
fosterm-mw <michael.foster@mavenwave.com>
2022-06-17 23:31:28 -0500

    reorganizing dir

--- network.tf
+++
@@ -1,8 +0,0 @@
-module "vpc" {
-  source                                 = "terraform-google-modules/network/google//modules/vpc"
-  project_id                             = "terraform-ignite"
-  network_name                           = "terraform-test"
-  routing_mode                           = "REGIONAL"
-  shared_vpc_host                        = false
-  delete_default_internet_gateway_routes = false
-}

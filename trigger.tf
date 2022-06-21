16912e97465a80f65026affa5c57cad2b8551d1b
fosterm-mw <michael.foster@mavenwave.com>
2022-06-21 10:07:40 -0500

    ignite template for ci

--- trigger.tf
+++ trigger.tf
@@ -1,4 +1,11 @@
-resource "google_cloudbuild_trigger" "react-trigger" {
+resource "google_cloudbuild_trigger" "cloudbuild_triggers" {
+	for_each = var.user_tf ? toset(var.app_names) : toset([])
+
+	project	= var.gcp_project_id
+	name	= "${each.key}-ci"
+
+	disabled = !var.use_gke
+	
 	github {
 		owner	= "fostemi"
 		name	= "terraform-ignite"
@@ -10,5 +17,15 @@ resource "google_cloudbuild_trigger" "react-trigger" {
 	ignored_files	= [".gitignore", "*.tf"]
 	
 	filename	= "/angular-tour-of-heroes/cloudbuild.yaml"
+
+	substitutions = {
+		_APP_NAME	= each.key
+		_DOMAIN		= "${each.key}.${var.domain}"
+		_REGION		= local.region
+		_ZONE		= local.zone
+		_CLUSTER	= local.cluster_name
+		_TENANT_CODE= split("-", var.repo_name)[1]
+	}
+
 }
 
